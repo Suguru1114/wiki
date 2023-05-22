48d1f6991e58c3e1beb8eb7c9388aba437c0c7de
Suguru Miyata <suguru1114@gmail.com>
2023-05-22 13:04:55 +1000

    trying to fix viewa.py

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,16 +8,24 @@ def index(request):
         "entries": util.list_entries()
     })
 def entry(request, title):
-    content = util.get_entry(title)
-    if content is None:
-        # Handle the case when the entry does not exist
-        return render(request, "encyclopedia/error.html", {
-            "message": "Page not found"
-        })
-    else:
-        # Display the entry content
-        return render(request, "encyclopedia/entry.html", {
-            "title": title,
-            "content": content
-        })
+# def entry(request, title):
+#     content = util.get_entry(title)
+#     if content is None:
+#         # Handle the case when the entry does not exist
+#         return render(request, "encyclopedia/error.html", {
+#             "message": "Page not found"
+#         })
+#     else:
+#         # Display the entry content
+#         return render(request, "encyclopedia/entry.html", {
+#             "title": title,
+#             "content": content
+#         })
+
+def index(request):
+    entries = util.list_entries()
+    return render(request, "encyclopedia/index.html", {
+        "entries": entries
+    })
+
 

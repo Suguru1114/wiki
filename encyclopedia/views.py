c479ec50240522a3487685fd5a97a9651fa1aa9a
Suguru Miyata <suguru1114@gmail.com>
2023-05-22 21:02:54 +1000

    add new file to fix

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -7,25 +7,10 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
 def entry(request, title):
-# def entry(request, title):
-#     content = util.get_entry(title)
-#     if content is None:
-#         # Handle the case when the entry does not exist
-#         return render(request, "encyclopedia/error.html", {
-#             "message": "Page not found"
-#         })
-#     else:
-#         # Display the entry content
-#         return render(request, "encyclopedia/entry.html", {
-#             "title": title,
-#             "content": content
-#         })
-
-def index(request):
-    entries = util.list_entries()
-    return render(request, "encyclopedia/index.html", {
-        "entries": entries
-    })
-
-
+    content = util.get_entry(title)
+    if content is None:
+        return render(request, "encyclopedia/error.html", {"message": "Page not found"})
+    else:
+        return render(request, "encyclopedia/entry.html", {"title":title, "content":content})

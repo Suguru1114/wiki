f336d4524e2c5a472e7bbaa9dfc5cd5569f35b60
Suguru Miyata <suguru1114@gmail.com>
2023-05-29 21:13:06 +1000

    try search function

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -18,25 +18,41 @@ def entry(request, title):
     else:
         return render(request, "encyclopedia/index.html", {"title":title, "content":content})
     
+# def index(request):
+#     if request.method == "GET":
+#         query = request.GET.get('q', '')    
+#         entries = util.list_entries()
+        
+#         if query:
+#             # Filter the entries based on the search query
+#             entries = [entry for entry in entries if query.lower() in entry.lower()]
+#         return render(request, "encyclopedia/index.html", {"entries": entries, "query": query})
+#     else:
+#         return redirect('index')
+
+
+from django.shortcuts import render, redirect
+from . import util
+
 def index(request):
     if request.method == "GET":
-        query = request.GET.get('q', '')    
+        query = request.GET.get('q', '')
         entries = util.list_entries()
+
         if query:
             # Filter the entries based on the search query
-            entries = [entry for entry in entries if query.lower() in entry.lower()]
+            filtered_entries = [entry for entry in entries if query.lower() in entry.lower()]
+
+            # Check if the search query matches any entry
+            if filtered_entries:
+                # Retrieve the first matched entry
+                first_entry = filtered_entries[0]
+                content = util.get_entry(first_entry)
+
+                return render(request, "encyclopedia/entry.html", {"title": first_entry, "content": content})
+            else:
+                return render(request, "encyclopedia/error.html", {"message": "Page not found"})
+
         return render(request, "encyclopedia/index.html", {"entries": entries, "query": query})
     else:
         return redirect('index')
-
-# def entry_page(request, title):
-#     entry = get_object_or_404(Entry, title=title)
-
-#     if entry:
-#         # Entry exists, render the entry template
-#         return render(request, 'encyclopedia/entry.html', {'entry': entry})
-#     else:
-#         # Entry doesn't exist, redirect to the error page
-#         return redirect('error')
-
-# add search function 

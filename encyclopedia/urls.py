728abc09ef6f308d97e588f2975589b36416c808
Suguru Miyata <suguru1114@gmail.com>
2023-06-03 15:33:37 +1000

    start from 0

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,16 +2,6 @@ from django.urls import path
 
 from . import views
 
-from encyclopedia import views
-
 urlpatterns = [
-    path("", views.index, name="index"),
-    # path("wiki/<str:title>", views.entry, name="entry"),
-    path('entry/<str:title>/', views.entry, name='entry'),
-    # path('entry/<str:title>/', views.entry_page, name='entry'),
-    # path('error/', views.error_page, name='error'),
-    # from stackoverflow
-    # path("search", views.search, name="search"),
-    
- ]
-  
+    path("", views.index, name="index")
+]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,45 +1,22 @@
-from django.http import HttpResponseRedirect
 from django.shortcuts import render
-# from django.shortcuts import render, get_object_or_404, redirect
 from . import util
-from django.shortcuts import render, redirect
-import markdown
+# import markdown
 
 
 
-# def index(request):
-#     return render(request, "encyclopedia/index.html", {
-#         "entries": util.list_entries()
-#     })
-
-def convert_md_to_html(title):
-    content = util.get_entry(title)
-    markdowner = markdown.Markdown()
-    if content == None:
-        return None
-    else:
-        return markdowner.convert(content)
-
-def entry(request, title):
-    content = util.get_entry(title)
-    if content is None:
-        return render(request, "encyclopedia/error.html", {"message": "Page not found"})
-    else:
-        return render(request, "encyclopedia/entry.html", {"title":title, "content":content})
+# def convert_md_to_html(title):
+#     content = util.get_entry(title)
+#     markdowner = markdown.Markdown()
+#     if content == None:
+#         return None
+#     else:
+#         return markdowner.convert(content)
 
 
 def index(request):
-    if request.method == "GET":
-        query = request.GET.get('q', '')
-        entries = util.list_entries()
-        if query:
-            # Filter the entries based on the search query
-            entries = [entry for entry in entries if query.lower() in entry.lower()]
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
 
-            # If there is an exact match for the search query, redirect to the entry page
-            if query.lower() in [entry.lower() for entry in entries]:
-                return redirect('entry', title=query)
 
-        return render(request, "encyclopedia/index.html", {"entries": entries, "query": query})
-    # else:
-    #     return redirect('index')
+

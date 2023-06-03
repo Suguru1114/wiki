658dc44c50748934f33c58e690f1f9d5619f11d1
Suguru Miyata <suguru1114@gmail.com>
2023-06-03 17:09:49 +1000

    fix

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 from . import util
-# import markdown
+import markdown
 
 
 
-# def convert_md_to_html(title):
-#     content = util.get_entry(title)
-#     markdowner = markdown.Markdown()
-#     if content == None:
-#         return None
-#     else:
-#         return markdowner.convert(content)
+def convert_md_to_html(title):
+    content = util.get_entry(title)
+    markdowner = markdown.Markdown()
+    if content == None:
+        return None
+    else:
+        return markdowner.convert(content)
 
 
 def index(request):

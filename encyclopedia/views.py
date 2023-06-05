d08b758c9cd62ec3272e0e4b415ec23b7a4b0f23
Suguru Miyata <suguru1114@gmail.com>
2023-06-05 13:30:01 +1000

    add urls

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -23,7 +23,7 @@ def convert_md_to_html(title):
 
 def entry(request, title):
     content = util.get_entry(title)
-    if content is None:
+    if content == None:
         return render(request, "encyclopedia/error.html", {"message": "Page not found"})
     else:
         return render(request, "encyclopedia/entry.html", {"title":title, "content":content})

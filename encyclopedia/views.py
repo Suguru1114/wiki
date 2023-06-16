6b517cd2d2ad5e683b3189b7e9222280853868d3
Suguru Miyata <suguru1114@gmail.com>
2023-06-16 13:12:03 +1000

    fix add

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # from django.shortcuts import render, get_object_or_404, redirect
 from . import util
 from django.shortcuts import render, redirect
-import markdown
+# import markdown
 from django import forms
 
 
